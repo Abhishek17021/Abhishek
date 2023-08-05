7e3827b8f5dd4761cb02c504b2839c06000bf041
parom <parom@LAPTOP-0SROM74D>
2023-08-05 13:00:57 +0530

    Add project files.

---
+++ Abhishek/EmployeeData/DataEmployee.cs
@@ -0,0 +1,15 @@
+﻿using Abhishek.Models;
+
+namespace Abhishek.EmployeeData
+{
+    public class DataEmployee
+    {
+        public static List<Employee> employees = new List<Employee>()
+        {
+        new Employee() { Id = 1,Name= "Kavita", Location= "Kolkata"},
+        new Employee() { Id = 2,Name= "Piyush", Location="Pune" },
+        new Employee() { Id = 3,Name= "Mansi", Location="Hyd" },
+
+     };
+    }
+}
